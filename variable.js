0f7a5e8a627ae6fb5a966f8cd6b170c439055011
MeganNguyen1 <139710259+MeganNguyen1@users.noreply.github.com>
2025-01-24 19:39:09 -0600

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,32 @@
+let favSnack = "Chips";
+console.log(favSnack);
+
+let name = "Megan";
+let age = 11;
+let country = "USA";
+let bookReads = 5;
+let isHappy = true;
+//logging to the console
+
+console.log("I feel very "+isHappy + ". My name is "+name +". My age is "+age +". I live in the "+country +". The number of books i've read is "+bookReads );
+
+console.log( );
+
+let place = "cafe"
+let barista = "barista"
+let cashier = "cashier"
+let option1 = "mocha"
+let option2 = "americano"
+let option3 = "latte"
+
+console.log("The place people come to drink coffee is a "+place +". The person who makes the drinks is the "+barista +". The person who takes the orders is the "+cashier +". The drink that has coffee and chocolate is called a "+option1 +". The drink that has coffee and water is a "+option2 +". The drink that has coffee and milk is a "+option3 );
+
+console.log( );
+
+let number1 = 10;
+let number2 = 20;
+
+console.log("Sum: ", number1 + number2);
+console.log("Difference: ", number2 - number1);
+console.log("Product: ", number1 * number2);
+console.log("Quotient: ", number2 / number1);
