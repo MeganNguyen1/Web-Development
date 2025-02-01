0e7e998cad4a984713649bbccd8d9517ae627bb1
MeganNguyen1 <139710259+MeganNguyen1@users.noreply.github.com>
2025-01-31 19:30:49 -0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,20 @@
+console.log(" /\\_/\\ ")
+console.log("( o.o )")
+console.log(" > ^ <")
+
+console.log( )
+
+let name="Megan" 
+let age= 11;
+let country="USA";
+let favFood="noddles";
+
+console.log("My name is "+name +". My age is "+age +". I live in the "+country +". My favorite food is "+favFood);
+
+let num1 = 20;
+let num2 = 30;
+
+console.log("Sum: ",num1 + num2);
+console.log("Difference: ",num1 - num2);
+console.log("Product: ",num1 * num2);
+console.log("Quotient: ",num1 / num2);
