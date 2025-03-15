2f6866897b79b0de92fdc833e703d3f08171f02b
MeganNguyen1 <139710259+MeganNguyen1@users.noreply.github.com>
2025-03-14 19:26:43 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,9 @@
+console.log("Hello World!")
+
+for (counter = 1; counter<=5; counter++ ){
+    console.log(" 🌟 ",counter );
+}
+
+for (counter = 1; counter<=10; counter++ ){
+    console.log(" I love coding! ",counter );
+}
