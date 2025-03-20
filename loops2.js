c86d831d2830fe2e121474dc14c4c4575669446e
MeganNguyen1 <139710259+MeganNguyen1@users.noreply.github.com>
2025-03-19 19:25:45 -0500

    Add files via upload

---
+++ loops2.js
@@ -0,0 +1,16 @@
+for(counter = 1; counter <=10; counter+=2){
+    console.log("even number", counter)
+}
+
+for(counter = 2; counter <=10; counter+=2){
+    console.log("odd number", counter)
+}
+
+for(counter = 1; counter <=10; counter+=3){
+    console.log("3 times", counter)
+}
+
+console.log("Multiplication table of 3")
+for(counter = 1; counter <=10; counter++){
+    console.log("", counter*3)
+}
