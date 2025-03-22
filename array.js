43c6a221cd6411d657d81b68cffd7d03792e6634
MeganNguyen1 <139710259+MeganNguyen1@users.noreply.github.com>
2025-03-21 19:07:47 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,27 @@
+let candies = ["Vanilla", "Strawberry", "Chocolate", "Mint"];
+
+console.log(candies[3]);
+console.log(candies[2]);
+console.log(candies[1]);
+console.log(candies[0]);
+
+let student_ages = [5, 7, 6, 7, 9, 10, 9, 5, 7, 10]
+
+console.log(student_ages[0])
+console.log(student_ages[1])
+console.log(student_ages[2])
+console.log(student_ages[3])
+console.log(student_ages[4])
+console.log(student_ages[5])
+console.log(student_ages[6])
+console.log(student_ages[7])
+console.log(student_ages[8])
+console.log(student_ages[9])
+
+let colors = ["Red", "Orange", "Yellow", "Green", "Blue"]
+
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
+console.log(colors[3])
+console.log(colors[4])
