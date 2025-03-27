2ae7931086abd26afc0cf4b8bfcec37fa6e9aaf4
MeganNguyen1 <139710259+MeganNguyen1@users.noreply.github.com>
2025-03-26 19:35:11 -0500

    Add files via upload

---
+++ pushAndPop.js
@@ -0,0 +1,12 @@
+let fruit = ["apple", "banana"]
+console.log(fruit)
+
+fruit.push("mango");
+console.log(fruit);
+
+fruit.pop();
+console.log(fruit);
+
+let toys = ["Car", "Doll", "Blocks"]
+toys.pop()
+console.log(toys)
