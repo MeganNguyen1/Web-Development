c655a3416be7c86d5cfacffd6046563c5e0f3eb7
MeganNguyen1 <139710259+MeganNguyen1@users.noreply.github.com>
2025-03-28 19:28:12 -0500

    Add files via upload

---
+++ pushPopTask.js
@@ -0,0 +1,10 @@
+/*TASK: Last Student Leaves the Classroom
+Question:
+Create an array called students with names: ["Alice", "Bob", "Charlie"].
+Use pop() until only one student is left in the array.
+Print the names of the students who left. */
+
+let students = ["Alice", "Bob", "Charlie"]
+students.pop();
+students.pop();
+console.log(students)
