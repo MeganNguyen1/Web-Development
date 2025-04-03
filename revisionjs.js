136ca01f1b1a231b307f27de755d2cf98e8ed0b4
MeganNguyen1 <139710259+MeganNguyen1@users.noreply.github.com>
2025-04-02 19:39:05 -0500

    Add files via upload

---
+++ revisionjs.js
@@ -0,0 +1,15 @@
+console.log("HELLO WORLD")
+console.log("Megan")
+let name = ("Megan")
+let age = 11
+let course = ("Coding")
+let grade = 5
+let hobby = ("Readimg")
+console.log("My name is "+name +"My course is "+course+ "My hobby is "+hobby)
+console.log("My age is ", age)
+
+let players =  ["Leo", "Mia", "Noah", "Emma"]
+players.pop()
+players.pop()
+players.pop()
+console.log(players)
